899aed30981edfa1cecfb6789e7394a1b27ce3c1
upc-ghy <15155096254@163.com>
2024-01-13 20:34:50 +0800

    upload KNN source code

---
+++ KNN/README.md
@@ -0,0 +1,25 @@
+# Using CUDA programming for parallel acceleration, efficient K-nearest neighbors (KNN) can be implemented
+The testing environment is: pytorch, python3+, CUDA programming
+
+# dependency
+Tested with cuda 11.7, python 3.7.16, pytorch 1.13.1, gcc 11.2.0
+
+# installation
+
+~~~shell
+# clone the tool project
+git clone https://github.com/upc-ghy/KNN.git
+# cd KNN
+cd KNN/
+# install
+sudo python setup.py install
+~~~
+
+# A Simple Use Case
+
+~~~shell
+python test.py
+~~~
+
+
+
---
+++ KNN/test.py
@@ -0,0 +1,38 @@
+import unittest
+import torch
+from torch.autograd import Variable, Function
+
+from vision_sandbox import _C
+knn = _C.knn
+
+class TestKNearestNeighbor(unittest.TestCase):
+
+    def test_forward(self):
+        # use CPU
+        ref = Variable(torch.rand(1, 3, 300))
+        query = Variable(torch.rand(1, 3, 300))
+        ref = ref.float()
+        query = query.float()
+
+        inds = torch.empty(query.shape[0], 1, query.shape[2]).long()
+        
+        print("CPU")
+        knn(ref, query, inds)
+
+        print(query)
+        print(ref)
+        print(inds.shape)
+        print(inds)
+        
+        # use GPU
+        print("CUDA")
+        ref = ref.cuda()
+        query = query.cuda()
+        inds = inds.cuda()
+        knn(ref, query, inds)
+        print(inds)
+
+
+
+if __name__ == '__main__':
+  unittest.main()
