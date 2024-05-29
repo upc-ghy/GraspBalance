c4d2b694b2e31125595ebc743ed4aba6295b5998
upc-ghy <133858812+upc-ghy@users.noreply.github.com>
2024-05-29 13:30:12 +0800

    Update pointnet2_utils.py

--- PointNet/pointnet2_utils.py
+++ PointNet/pointnet2_utils.py
@@ -1,9 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# 
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
 
-''' Modified based on: https://github.com/erikwijmans/Pointnet2_PyTorch '''
 from __future__ import (
     division,
     absolute_import,
